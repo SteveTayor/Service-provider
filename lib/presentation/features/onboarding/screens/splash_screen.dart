507a7858769121a06c63415963f447602c6c7a3f
SteveTayor <ayomidearose75@gmail.com>
2025-08-07 13:14:55 +0100

    `feat: Update app version and version code in various places`

--- lib/presentation/features/onboarding/screens/splash_screen.dart
+++ lib/presentation/features/onboarding/screens/splash_screen.dart
@@ -1,8 +1,9 @@
+import 'package:bundlegram/core/config/constants.dart';
 import 'package:bundlegram/core/utils/colors.dart';
-import 'package:bundlegram/core/router/app_router.dart';
 import 'package:bundlegram/core/router/route_constants.dart';
 import 'package:bundlegram/gen/assets.gen.dart';
 import 'package:bundlegram/presentation/general_widget/app_svg.dart';
+import 'package:bundlegram/data/datasources/local/secure_storage_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -14,14 +15,46 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  void _goToWalkThrough() {
-    context.go(RouteConstants.walkThrough);
-  }
-
+  late final SecureStorageHelper _storage;
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 5), _goToWalkThrough);
+    _initializeApp();
+  }
+
+  Future<void> _initializeApp() async {
+    await clearIfAppUpdated(); // Step 1: version check
+    await Future.delayed(const Duration(seconds: 2)); // Optional splash delay
+    _goToWalkThrough(); // Step 2: navigate
+  }
+
+  Future<bool> clearIfAppUpdated() async {
+    final currentVersionCode = int.tryParse(AppConstants.appBuildNumber) ?? 0;
+    final storedVersion = await _storage.getAppVersionCode();
+
+    if (storedVersion == null || storedVersion < currentVersionCode) {
+      await _storage.clearAll();
+      await _storage.setAppVersionCode(currentVersionCode);
+      return true; // Was updated
+    }
+
+    return false; // No update
+  }
+
+  // Future<void> _checkAndResetOnUpdate() async {
+  //   final storedVersionCodeStr = await _storage.read('app_version_code');
+  //   final storedVersionCode = int.tryParse(storedVersionCodeStr ?? '0');
+
+  //   if (storedVersionCode == null ||
+  //       storedVersionCode < AppVersion.versionCode) {
+  //     await _storage.clearAll();
+  //     await _storage.write(
+  //         'app_version_code', AppVersion.versionCode.toString());
+  //   }
+  // }
+
+  void _goToWalkThrough() {
+    context.go(RouteConstants.walkThrough);
   }
 
   @override
@@ -29,15 +62,13 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
       body: Center(
-          child: AppSvgIcon(
-        width: 150,
-        height: 150,
-        path: Assets.svgs.bundlegramWhiteLogo,
-        color: AppColors.background,
-      )
-          // Image.asset(Assets.images.logo.path),
-
-          ),
+        child: AppSvgIcon(
+          width: 150,
+          height: 150,
+          path: Assets.svgs.bundlegramWhiteLogo,
+          color: AppColors.background,
+        ),
+      ),
     );
   }
 }
