46887fd419e59f82865ca0cca026d20fde8eeefe
SteveTayor <ayomidearose75@gmail.com>
2025-09-02 06:57:51 +0100

    `Bump app version to 1.0.8 and update corresponding version codes and constants.`

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -7,10 +7,10 @@ class AppConstants {
   static const String appName = 'Bundlegram';
 
   /// App version
-  static const String appVersion = '1.0.7';
+  static const String appVersion = '1.0.8';
 
   /// App build number
-  static const String appBuildNumber = '7';
+  static const String appBuildNumber = '8';
 
   /// Default animation duration
   static const Duration defaultAnimationDuration = Duration(milliseconds: 300);
