cfb12d2f5d03d142532d06dfa16855c0bfb7e3cf
SteveTayor <ayomidearose75@gmail.com>
2025-09-11 18:11:14 +0100

    `Added Firebase configuration and plugins for analytics, messaging, and local notifications.`

---
+++ lib/services/firebase_sevice/fcm_background_handler.dart
@@ -0,0 +1,23 @@
+import 'package:bundlegram/services/notification_services/notification_services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+
+Future<void> fcmBackgroundHandler(RemoteMessage message) async {
+  // initialize Firebase if you plan to use other Firebase services here
+  await Firebase.initializeApp();
+  if (kDebugMode) {
+    debugPrint('*** FCM bg message: ${message.messageId}');
+    debugPrint('data: ${message.data}');
+    debugPrint(
+        'notification: ${message.notification?.title} / ${message.notification?.body}');
+  }
+
+  // You can display a local notification from background if safe on the platform:
+  try {
+    await NotificationService().displayPushNotification(message);
+  } catch (e) {
+    if (kDebugMode)
+      debugPrint('Error while showing local notification in bg: $e');
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,9 @@ import Foundation
 
 import connectivity_plus
 import device_info_plus
+import firebase_core
+import firebase_messaging
+import flutter_local_notifications
 import flutter_secure_storage_macos
 import geolocator_apple
 import network_info_plus
@@ -20,6 +23,9 @@ import webview_flutter_wkwebview
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
