b9d2cd0fc16b57ec3e40a8e14585fb3c33873303
SteveTayor <ayomidearose75@gmail.com>
2025-12-01 23:19:23 +0100

    feat: Add GetAllBeneficiariesResponse model and related JSON serialization methods

---
+++ lib/data/models/beneficiaries/get_all_beneficiaries.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+GetAllBeneficiariesResponse getAllBeneficiariesResponseFromJson(String str) =>
+    GetAllBeneficiariesResponse.fromJson(
+        json.decode(str) as Map<String, dynamic>);
+
+String getAllBeneficiariesResponseToJson(GetAllBeneficiariesResponse data) =>
+    json.encode(data.toJson());
+
+class GetAllBeneficiariesResponse {
+  final String? status;
+  final List<Beneficiary>? data;
+  final String? message;
+
+  GetAllBeneficiariesResponse({
+    this.status,
+    this.data,
+    this.message,
+  });
+
+  factory GetAllBeneficiariesResponse.fromJson(Map<String, dynamic> json) =>
+      GetAllBeneficiariesResponse(
+        status: json["status"] as String?,
+        data: json["data"] == null
+            ? <Beneficiary>[]
+            : List<Beneficiary>.from((json["data"] as List<dynamic>)
+                .map<Beneficiary>(
+                    (x) => Beneficiary.fromJson(x as Map<String, dynamic>))),
+        message: json["message"] as String?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        "status": status,
+        "data": data == null
+            ? []
+            : List<dynamic>.from(data!.map((x) => x.toJson())),
+        "message": message,
+      };
+}
+
+class Beneficiary {
+  final int? id;
+  final String? network;
+  final String? phoneNumber;
+
+  Beneficiary({
+    this.id,
+    this.network,
+    this.phoneNumber,
+  });
+
+  factory Beneficiary.fromJson(Map<String, dynamic> json) => Beneficiary(
+        id: json["id"] as int?,
+        network: json["network"] as String?,
+        phoneNumber: json["phone_number"] as String?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "network": network,
+        "phone_number": phoneNumber,
+      };
+}
--- lib/presentation/features/wallet/screen/wallet_screen.dart
+++ lib/presentation/features/wallet/screen/wallet_screen.dart
@@ -26,7 +26,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
+// import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class WalletScreen extends ConsumerStatefulWidget {
   const WalletScreen({super.key});
